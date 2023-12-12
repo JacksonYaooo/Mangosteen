2f1635079c634de8d664ee177ea57de28d7d970d
JacksonYaooo <2594838054@qq.com>
2023-12-12 23:10:06 +0800

    feat: 创建ValidationCodeController

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,8 @@ Rails.application.routes.draw do
 
   namespace :api do
     namespace :v1 do
-      #/api/v1
-      resources :validation_code, only: [:create]
+      # /api/v1
+      resources :validation_codes, only: [:create]
       resource :session, only: [:create, :destroy]
       resource :me, only: [:show]
       resources :items
