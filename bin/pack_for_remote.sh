3d23ce75260e45089307ffafc90f8c12dea851b2
JacksonYaooo <2594838054@qq.com>
2024-03-05 21:52:39 +0800

    revert: 复原到之前

--- bin/pack_for_remote.sh
+++ bin/pack_for_remote.sh
@@ -9,8 +9,7 @@ current_dir=$(dirname $0)
 deploy_dir=/home/$user/deploys/$time
 gemfile=$current_dir/../Gemfile
 gemfile_lock=$current_dir/../Gemfile.lock
-vendor_dir=$current_dir/../vendor
-vendor_1=rspec_api_documentation
+vendor_cache_dir=$current_dir/../vendor/cache
 
 function title {
   echo 
@@ -20,24 +19,19 @@ function title {
   echo 
 }
 
-mkdir -p $cache_dir
-title '打包源代码'
-tar --exclude="tmp/cache/*" --exclude="tmp/deploy_cache/*" --exclude="vendor/*" -cz -f $dist *
-title "打包本地依赖 ${vendor_1}"
-bundle cache --quiet
-tar -cz -f "$vendor_dir/cache.tar.gz" -C ./vendor cache
-tar -cz -f "$vendor_dir/$vendor_1.tar.gz" -C ./vendor $vendor_1
+
+title '打包源代码为压缩文件'
+mkdir $cache_dir
+bundle cache
+tar --exclude="tmp/cache/*" --exclude="tmp/deploy_cache/*" -czv -f $dist *
 title '创建远程目录'
 ssh $user@$ip "mkdir -p $deploy_dir/vendor"
-title '上传源代码和依赖'
+title '上传压缩文件'
 scp $dist $user@$ip:$deploy_dir/
 yes | rm $dist
 scp $gemfile $user@$ip:$deploy_dir/
 scp $gemfile_lock $user@$ip:$deploy_dir/
-scp -r $vendor_dir/cache.tar.gz $user@$ip:$deploy_dir/vendor/
-yes | rm $vendor_dir/cache.tar.gz
-scp -r $vendor_dir/$vendor_1.tar.gz $user@$ip:$deploy_dir/vendor/
-yes | rm $vendor_dir/$vendor_1.tar.gz
+scp -r $vendor_cache_dir $user@$ip:$deploy_dir/vendor/
 title '上传 Dockerfile'
 scp $current_dir/../config/host.Dockerfile $user@$ip:$deploy_dir/Dockerfile
 title '上传 setup 脚本'
--- bin/setup_remote.sh
+++ bin/setup_remote.sh
@@ -5,11 +5,10 @@ db_container_name=db-for-mangosteen
 
 function set_env {
   name=$1
-   hint=$2
+  hint=$2
   [[ ! -z "${!name}" ]] && return
   while [ -z "${!name}" ]; do
-    echo "> 请输入 $name:"
-    [[ ! -z "$hint" ]] && echo "> 请输入 $name: $hint" || echo "> 请输入 $name:" 
+    [[ ! -z "$hint" ]] && echo "> 请输入 $name: $hint" || echo "> 请输入 $name:"
     read $name
   done
   sed -i "1s/^/export $name=${!name}\n/" ~/.bashrc
@@ -26,7 +25,7 @@ function title {
 title '设置远程机器的环境变量'
 set_env DB_HOST
 set_env DB_PASSWORD
-set_env RAILS_MASTER_KEY '请将 config/credentials/production.key 的内容复制到这里'
+set_env RAILS_MASTER_KEY
 
 title '创建数据库'
 if [ "$(docker ps -aq -f name=^${DB_HOST}$)" ]; then
@@ -69,4 +68,4 @@ case $ans in
     ""     )  echo "no" ;;
 esac
 
-title '全部执行完毕'
+title '全部执行完毕
